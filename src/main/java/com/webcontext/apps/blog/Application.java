f695ead6c142aecaf5b6691245f3a5962b55a9ef
Frédéric Delorme <frederic.delorme@gmail.com>
2015-11-24 00:16:57 +0100

    create blog project

---
+++ src/main/java/com/webcontext/apps/blog/Application.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package com.webcontext.apps.blog;
+
+/**
+ * @author frederic
+ *
+ */
+@SpringBootApplication
+public class Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+}
