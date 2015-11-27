ebe79829c879bbb26bb0c0880452ecc06ff4a1ef
Frédéric Delorme <frederic.delorme@gmail.com>
2015-11-28 00:48:31 +0100

    Update and make running all IT tests.

--- src/test/java/com/webcontext/apps/blog/tests/PostControllerTestIT.java
+++ src/test/java/com/webcontext/apps/blog/tests/PostControllerTestIT.java
@@ -25,6 +25,7 @@ import org.springframework.web.client.RestTemplate;
 
 import com.jayway.restassured.RestAssured;
 import com.jayway.restassured.builder.ResponseSpecBuilder;
+import com.jayway.restassured.http.ContentType;
 import com.jayway.restassured.specification.ResponseSpecification;
 import com.webcontext.apps.blog.Application;
 import com.webcontext.apps.blog.model.Post;
@@ -49,8 +50,9 @@ public class PostControllerTestIT {
 	private Date today;
 	private Post post0, post1, post2, post3;
 
-    @Value("${local.server.port}")
-    int port=0;
+	@Value("${local.server.port}")
+	int port = 0;
+
 	/**
 	 * @throws java.lang.Exception
 	 */
@@ -62,33 +64,25 @@ public class PostControllerTestIT {
 		post2 = new Post("title2", "cover", "header2", "content2", today, "Soso", PublicationState.VALIDATED);
 		post3 = new Post("title3", "cover", "header3", "content3", today, "Nath", PublicationState.PUBLISHED);
 		postRepository.save(Arrays.asList(post0, post1, post2, post3));
-		
+
 		RestAssured.port = port;
 	}
 
 	@Test
-	public void canFindAll(){
-		when()
-			.get("/posts")
-		.then()
-			.statusCode(HttpStatus.SC_OK)
-			.body("title", Matchers.hasItems("title0", "title1", "title2","title3"));
+	public void canFindAllPost() {
+		when().get("/posts").then().statusCode(HttpStatus.SC_OK).body("title",
+				Matchers.hasItems("title0", "title1", "title2", "title3"));
 	}
-	
+
 	/**
-	 * Test method for {@link com.webcontext.apps.blog.services.PostController#findById(long)}.
+	 * Test method for
+	 * {@link com.webcontext.apps.blog.services.PostController#findById(long)}.
 	 */
 	@Test
-	public void canFindById() {
+	public void canFindPostById() {
 		Integer id = post0.getId();
-		when()
-			.get("/posts/{id}",id)
-		.then()
-			.statusCode(HttpStatus.SC_OK)
-			.body("id",Matchers.is(id));
+		when().get("/posts/{id}", id).then().statusCode(HttpStatus.SC_OK).body("id", Matchers.is(id));
 	}
-	
-	
 
 	/**
 	 * Test method for
@@ -96,9 +90,9 @@ public class PostControllerTestIT {
 	 * .
 	 */
 	@Test
-	public void canAddItem() {
-		String body="{\"title\":\"added\",\"cover\":\"addedcover\",\"header\":\"addedheader\",\"content\":\"addedcontent\",\"createdAt\":\""+new Date()+"\",\"createdBy\":\"McGivrer\",\"status\":\""+PublicationState.DRAFT+"\"}";
-		expect().statusCode(HttpStatus.SC_OK).given().body(body).post("/posts");
+	public void canAddNewPost() {
+		Post addedPost = new Post("title", "cover", "header", "content", new Date(), "McG", PublicationState.DRAFT);
+		expect().statusCode(HttpStatus.SC_OK).given().body(addedPost).contentType(ContentType.JSON).post("/posts");
 	}
 
 	/**
@@ -107,8 +101,10 @@ public class PostControllerTestIT {
 	 * .
 	 */
 	@Test
-	public void canUpdatePost() {
-		//
+	public void canUpdateExistingPost() {
+		post1.setContent("contentmodified");
+		expect().statusCode(HttpStatus.SC_OK).given().body(post1).contentType(ContentType.JSON).put("/posts/{id}",
+				post1.getId());
 	}
 
 	/**
@@ -117,13 +113,10 @@ public class PostControllerTestIT {
 	 * .
 	 */
 	@Test
-    public void canDeletePost0() {
-        Integer postId = post0.getId();
- 
-        when()
-                .delete("/characters/{id}", postId).
-        then().
-                statusCode(HttpStatus.SC_NO_CONTENT);
-    }
+	public void canDeletePost0() {
+		Integer postId = post0.getId();
+
+		when().delete("/posts/{id}", postId).then().statusCode(HttpStatus.SC_NO_CONTENT);
+	}
 
 }
