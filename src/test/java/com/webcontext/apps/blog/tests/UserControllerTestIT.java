dffde7bcc1b84c0ad9b37949981d81ec8c048b4b
Frédéric Delorme <frederic.delorme@ge.com>
2015-11-30 15:33:22 +0100

    Start adding User

---
+++ src/test/java/com/webcontext/apps/blog/tests/UserControllerTestIT.java
@@ -0,0 +1,122 @@
+/**
+ * 
+ */
+package com.webcontext.apps.blog.tests;
+
+import static com.jayway.restassured.RestAssured.expect;
+import static com.jayway.restassured.RestAssured.when;
+
+import java.util.Date;
+
+import org.apache.http.HttpStatus;
+import org.hamcrest.Matchers;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.IntegrationTest;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.TestRestTemplate;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.web.client.RestTemplate;
+
+import com.github.springtestdbunit.DbUnitTestExecutionListener;
+import com.github.springtestdbunit.annotation.DatabaseSetup;
+import com.jayway.restassured.RestAssured;
+import com.jayway.restassured.http.ContentType;
+import com.webcontext.apps.blog.Application;
+import com.webcontext.apps.blog.model.User;
+import com.webcontext.apps.blog.model.UserProfile;
+import com.webcontext.apps.blog.repositories.UserRepository;
+
+/**
+ * @author frederic
+ *
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Application.class)
+@WebAppConfiguration
+@ActiveProfiles("local")
+@IntegrationTest("server.port:0")
+@TestExecutionListeners({ DependencyInjectionTestExecutionListener.class, DirtiesContextTestExecutionListener.class,
+		TransactionalTestExecutionListener.class, DbUnitTestExecutionListener.class })
+@DatabaseSetup("/datasets/users.xml")
+public class UserControllerTestIT {
+
+	RestTemplate template = new TestRestTemplate();
+
+	@Autowired
+	UserRepository userRepo;
+
+	@Value("${local.server.port}")
+	int port = 0;
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		RestAssured.port = port;
+	}
+
+	@Test
+	public void canFindAllPost() {
+		when().get("/users").then().statusCode(HttpStatus.SC_OK).body("title", Matchers.hasItems("admin", "McGivrer"));
+	}
+
+	/**
+	 * Test method for {@link com.webcontext.apps.blog.services.PostController#findById(long)}.
+	 */
+	@Test
+	public void canFindPostById() {
+		when().get("/users/{username}", "McGivrer").then().statusCode(HttpStatus.SC_OK).body("firstname",
+				Matchers.is("Frédéric"));
+	}
+
+	/**
+	 * Test method for {@link com.webcontext.apps.blog.services.PostController#addItem(com.webcontext.apps.blog.model.Post)} .
+	 */
+	@Test
+	public void canAddNewPost() {
+		User usertoBeAdded = new User("test01", "test01pwd", "test@contact.com", "test01", "test01", new Date(),
+				new Date(), "admin", UserProfile.PUBLIC);
+		expect().statusCode(HttpStatus.SC_OK).given().body(usertoBeAdded).contentType(ContentType.JSON).post("/users");
+	}
+
+	@Test
+	public void canNotAddEmptyFieldsPost() {
+		User user = new User("", "", "", "", "", null, null, "", UserProfile.PUBLIC);
+		expect().statusCode(HttpStatus.SC_INTERNAL_SERVER_ERROR).given().body(user).contentType(ContentType.JSON)
+				.post("/users");
+
+	}
+
+	/**
+	 * Test method for
+	 * {@link com.webcontext.apps.blog.services.PostController#updateItem(com.webcontext.apps.blog.model.Post, java.lang.Long)} .
+	 */
+	@Test
+	public void canUpdateExistingPost() {
+		User userTobeUpdate = userRepo.findOne("admin");
+		System.out.println(userTobeUpdate);
+		userTobeUpdate.setFirstname("Frédéric");
+		expect().statusCode(HttpStatus.SC_OK).given().body(userTobeUpdate).contentType(ContentType.JSON)
+				.put("/users/{username}", "username");
+	}
+
+	/**
+	 * Test method for {@link com.webcontext.apps.blog.services.PostController#deleteItem(java.lang.Long)} .
+	 */
+	@Test
+	public void canDeletePost0() {
+		when().delete("/users/{username}", "McGivrer").then().statusCode(HttpStatus.SC_NO_CONTENT);
+	}
+
+}
